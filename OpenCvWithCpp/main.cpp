79bdb9374414db076db673b4e22a612aac5599e5
Arshad <dragonarshad@gmail.com>
2022-01-16 20:11:51 +0530

    Initial Commit

---
+++ OpenCvWithCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenCvWithCpp
+//
+//  Created by asfar on 16/01/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
